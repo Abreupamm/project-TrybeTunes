4f2d24270fd99457ac920438a6f294581d7e7485
pamela <abreupamm@gmail.com>
2022-05-18 19:04:32 -0300

    componet profileEdit e NotFound criado

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Search from './pages/Search';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
@@ -17,6 +19,8 @@ class App extends React.Component {
           <Album />
           <Favorites />
           <Profile />
+          <ProfileEdit />
+          <NotFound />
         </BrowserRouter>
       </div>
     );
--- src/componets/Content.js
+++ src/componets/Content.js
@@ -5,6 +5,8 @@ import Search from '../pages/Search';
 import Album from '../pages/Album';
 import Favorites from '../pages/Favorites';
 import Profile from '../pages/Profile';
+import ProfileEdit from '../pages/ProfileEdit';
+import NotFound from '../pages/NotFound';
 
 class Content extends React.Component {
   render() {
@@ -15,7 +17,8 @@ class Content extends React.Component {
       <Route path="/album/:id" component={Album} />
       <Route path="/favorites" component={Favorites} />
       <Route path="/profile" component={Profile} />   
-      
+      <Route path="/profile/edit" component={ProfileEdit} />
+      <Route path="/*" component={NotFound} />
       </main>
 
     );
