21cbf512bd9cfe797152c26e5ed8eb040e463018
pamela <abreupamm@gmail.com>
2022-05-23 18:02:23 -0300

    questão 8 feita

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends React.Component {
+  state = {
+    favorite: false,
+  }
+  handleOnClick = async () => {
+    const { musicFavorite } = this.props;
+    this.setState({ favorite: true });
+    const addMusic = await addSong(musicFavorite);
+    this.setState({ favorite: false });
+  }
+
   render() {
     const { trackName, previewUrl, trackId } = this.props;
+    const { favorite } = this.state;
     return (
       <div>
         <span>{ trackName }</span>
@@ -20,10 +32,11 @@ class MusicCard extends React.Component {
         <label
           htmlFor="favorita"
         >
-          Favotita
+          { favorite ? 'Carregando...' : 'Favorita' }
           <input
             data-testid={ `checkbox-music-${trackId}` }
             type="checkbox"
+            onClick={this.handleOnClick}
           />
         </label>
       </div>
@@ -35,6 +48,7 @@ MusicCard.propTypes = {
   trackName: PropTypes.string.isRequired,
   previewUrl: PropTypes.string.isRequired,
   trackId: PropTypes.number.isRequired,
+  musicFavorite: PropTypes.object.isRequired,
 };
 
 export default MusicCard;
