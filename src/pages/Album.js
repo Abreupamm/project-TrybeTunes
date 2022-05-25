6a2c08d074eab535597117b005f397845e0c53d5
pamela <abreupamm@gmail.com>
2022-05-25 10:53:11 -0300

    questão 11 refeita

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -62,6 +62,7 @@ class Album extends React.Component {
                     trackName={ music.trackName }
                     previewUrl={ music.previewUrl }
                     favoritesSongs={ favoritesSongs }
+                    UpdateSongList={ () => {} }
                   />);
                 }
                 return null;
